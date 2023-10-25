385f168e250e9e6df351f69b2f98d2979b51c791
BigTava <bigtavadev@gmail.com>
2023-10-25 17:00:19 +0100

    wip: adds product page

---
+++ src/components/Currency.vue
@@ -0,0 +1,17 @@
+<template>
+  {{ dollar(amt) }}
+</template>
+
+<script>
+export default {
+  props: ['amt'],
+  methods: {
+    dollar(value) {
+      return '$' + Number.parseFloat(value).toFixed(2)
+    }
+  },
+  name: 'CurrencyVue'
+}
+</script>
+
+<style></style>
